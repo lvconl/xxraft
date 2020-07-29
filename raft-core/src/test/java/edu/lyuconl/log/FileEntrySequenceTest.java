bb006af8fa68880193b1a1d9d1e369fd126d08dd
lvconl <835132735@qq.com>
2020-07-29 14:52:01 +0800

    add file entry sequence test

--- raft-core/src/test/java/edu/lyuconl/log/FileEntrySequenceTest.java
+++ raft-core/src/test/java/edu/lyuconl/log/FileEntrySequenceTest.java
@@ -1,9 +1,94 @@
 package edu.lyuconl.log;
 
+import edu.lyuconl.log.entry.Entry;
+import edu.lyuconl.log.entry.NoOpEntry;
+import edu.lyuconl.log.sequence.EntriesFile;
+import edu.lyuconl.log.sequence.EntryIndexFile;
+import edu.lyuconl.log.sequence.FileEntrySequence;
+import edu.lyuconl.support.ByteArraySeekableFile;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  *
  * @date 2020年7月28日时间
  * @author lyuconl
  */
 public class FileEntrySequenceTest {
+    private EntriesFile entriesFile;
+    private EntryIndexFile entryIndexFile;
+
+    @Before
+    public void setup() throws IOException {
+        entriesFile = new EntriesFile(new ByteArraySeekableFile());
+        entryIndexFile = new EntryIndexFile(new ByteArraySeekableFile());
+    }
+
+    @Test
+    public void testInitialize() throws IOException {
+        entryIndexFile.appendEntryIndex(1, 0L, 1,1);
+        entryIndexFile.appendEntryIndex(2, 20L, 1, 1);
+        FileEntrySequence sequence = new FileEntrySequence(entriesFile, entryIndexFile, 1);
+        Assert.assertEquals(3, sequence.getNextLogIndex());
+        Assert.assertEquals(1, sequence.getFirstLogIndex());
+        Assert.assertEquals(2, sequence.getLastLogIndex());
+        Assert.assertEquals(2, sequence.getCommitIndex());
+    }
+
+    @Test
+    public void testAppendEntry() {
+        FileEntrySequence sequence = new FileEntrySequence(entriesFile, entryIndexFile, 1);
+        Assert.assertEquals(1, sequence.getNextLogIndex());
+        sequence.append(new NoOpEntry(1, 1));
+        Assert.assertEquals(2, sequence.getNextLogIndex());
+        Assert.assertEquals(1, sequence.getLastEntry().getIndex());
+    }
+
+    private void appendEntryToFile(Entry entry) throws IOException {
+        long offset = entriesFile.appendEntry(entry);
+        entryIndexFile.appendEntryIndex(entry.getIndex(), offset, entry.getKind(), entry.getTerm());
+    }
+
+    @Test
+    public void testGetEntry() throws IOException {
+        appendEntryToFile(new NoOpEntry(1, 1));
+        FileEntrySequence sequence = new FileEntrySequence(entriesFile, entryIndexFile, 1);
+        sequence.append(new NoOpEntry(2, 1));
+        Assert.assertNull(sequence.getEntry(0));
+        Assert.assertEquals(1, sequence.getEntry(1).getIndex());
+        Assert.assertEquals(2, sequence.getEntry(2).getIndex());
+        Assert.assertNull(sequence.getEntry(3));
+    }
+
+    @Test
+    public void testSubList2() throws IOException {
+        appendEntryToFile(new NoOpEntry(1, 1));
+        appendEntryToFile(new NoOpEntry(2, 2));
+        FileEntrySequence sequence = new FileEntrySequence(entriesFile, entryIndexFile, 1);
+        sequence.append(new NoOpEntry(sequence.getNextLogIndex(), 3));
+        sequence.append(new NoOpEntry(sequence.getNextLogIndex(), 4));
+
+        List<Entry> subList = sequence.subList(2);
+        Assert.assertEquals(3, subList.size());
+        Assert.assertEquals(2, subList.get(0).getIndex());
+        Assert.assertEquals(4, subList.get(2).getIndex());
+    }
+
+    @Test
+    public void testRemoveAfterEntriesInFile2() throws IOException {
+        appendEntryToFile(new NoOpEntry(1, 1));
+        appendEntryToFile(new NoOpEntry(2, 1));
+        FileEntrySequence sequence = new FileEntrySequence(entriesFile, entryIndexFile, 1);
+
+        sequence.append(new NoOpEntry(3, 2));
+        Assert.assertEquals(1, sequence.getFirstLogIndex());
+        Assert.assertEquals(3, sequence.getLastLogIndex());
+        sequence.removeAfter(1);
+        Assert.assertEquals(1, sequence.getFirstLogIndex());
+        Assert.assertEquals(1, sequence.getLastLogIndex());
+    }
 }
